6e2b25087380faa0c0314c7b301557d31188ad02
Phil Swartz <pswartz.sju@gmail.com>
2019-09-21 08:44:07 -0400

    added the time calculations using moment javascript lib and appending the variables to the row, working on establishing a connection to the database next to store the data

--- app.js
+++ app.js
@@ -12,39 +12,63 @@ $('#submit-btn').on('click', function () {
     var first_train_time = $('#first-train-time-input').val().trim();
     var frequency = $('#frequency-input').val().trim();
 
-
+    // test logs
     console.log(train_name);
     console.log(destination);
     console.log(first_train_time);
     console.log(frequency);
 
     
+
+    // do math here
+    var firstTimeConverted = moment(first_train_time, 'HH:mm').subtract(1, 'years');
+    console.log(firstTimeConverted);
+
+    var currentTime = moment(); 
+    console.log(moment(currentTime).format('hh:mm'));
+
+    var timeDiff = moment().diff(moment(firstTimeConverted), 'minutes');
+    console.log('time diff ' + timeDiff);
+
+    var timeApart = timeDiff % frequency;
+    console.log('time apart ' + timeApart + ' minutes');
+
+    var minutesTillNextTrain = frequency - timeApart;
+    console.log('minutes until next train ' + minutesTillNextTrain)
+
+    var nextArrival = moment().add(minutesTillNextTrain, 'minutes');
+    console.log('ARRIVAL TIME ' + moment(nextArrival).format('hh:mm'));
+
+
+    // add row with new data
     var myRow = $('<tr>');
 
     $('.table-body').prepend(myRow);
 
     myRow.append('<td>' + train_name + '</td>');
     myRow.append('<td>' + destination + '</td>');
-    myRow.append('<td>' + frequency + '</td>');
+    myRow.append('<td id="table-row">' + frequency + '</td>');
 
     // table rows require calculations
-    myRow.append('<td>' + frequency + '</td>');
-    myRow.append('<td>' + frequency + '</td>');
-
-
-
-
-
-
+    myRow.append('<td id="table-row">' + moment(nextArrival).format('hh:mm') + '</td>');
+    myRow.append('<td id="table-row">' + minutesTillNextTrain + '</td>');
 
 
 
 
     // reset input box values back to their original state
-    $('#train-name-input').val('Enter a train name');
-    $('#destination-input').val('Enter travel destination');
-    $('#first-train-time-input').val('Enter a train time');
-    $('#frequency-input').val('Enter the frequency in minutes');
+    $('#train-name-input').val('');
+    $('#train-name-input').attr('placeholder', 'Enter a train name');
+
+    $('#destination-input').val('');
+    $('#destination-input').attr('placeholder', 'Enter travel destination');
+
+    $('#first-train-time-input').val('');
+    $('#first-train-time-input').attr('placeholder', 'Enter a train time');
+
+    $('#frequency-input').val('');
+    $('#frequency-input').attr('placeholder', 'Enter the frequency in minutes');
+
 
 
 });
