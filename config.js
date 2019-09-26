ccb2a9322520a0fc4a5307f7c9ba52d1a8f929b7
Phil Swartz <pswartz.sju@gmail.com>
2019-09-25 22:55:49 -0400

    commented out config file

---
+++ config.js
@@ -0,0 +1,10 @@
+
+
+/* var config = {
+    apiKey: "AIzaSyDw0MCF-Lg0y5QVndbGvEHDJC8JvWIJpqw",
+    messageSenderID: "339799422019",
+    appID: "1:339799422019:web:9034f013adb4812da710f6",
+
+} */
+
+// export default config
